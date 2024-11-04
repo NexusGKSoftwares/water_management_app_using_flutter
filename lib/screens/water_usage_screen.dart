ea84177f271c447a151c5f1a488651436aaa6d4a
NexusGk Softwares <nexusgksoftwares@gmail.com>
2024-11-04 17:42:56 +0300

    Added `const` constructors with `super.key` to `NotificationsScreen` and `WaterUsageScreen` widgets.

--- lib/screens/water_usage_screen.dart
+++ lib/screens/water_usage_screen.dart
@@ -4,6 +4,8 @@ class WaterUsageScreen extends StatelessWidget {
   // Sample data for daily water usage in liters
   final List<int> usageData = [70, 65, 50, 90, 40, 80, 75];
 
+  const WaterUsageScreen({super.key});
+
  
   @override
   Widget build(BuildContext context) {
